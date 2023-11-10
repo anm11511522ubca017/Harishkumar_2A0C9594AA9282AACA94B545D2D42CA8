e04dc4d2d1d176f7ec9279d372978a630982a317
anm11511522ubca017 <149796783+anm11511522ubca017@users.noreply.github.com>
2023-11-10 08:49:18 +0530

    Challenge 1.2
    I already upload challenge 1

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-#1.1 impliment a recursive function to calculate the factorial of the given number.
+# Python program to check if year is a leap year or not
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+year = 2000
 
-number = 2
-res = fact_rec(number)
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
 
-print("the factorial of {} is {} ".format (number,res))
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
